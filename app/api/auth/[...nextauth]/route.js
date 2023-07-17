4872cab55452e6c4934c56b136b01a9dc3e2a5ce
Jennifer Reuting <jenreuting@yahoo.com>
2023-07-16 17:13:27 -0700

    implemented Google auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,12 +2,12 @@ import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
-	// providers: [
-	// 	GoogleProvider({
-	// 		clientId: process.env.GOOGLE_CLIENT_ID,
-	// 		clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-	// 	}),
-	// ],
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+	],
 	pages: {
 		signIn: '/signin',
 	},
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Provider from './components/Provider';
+import Header from './components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
 		>
 			<body className='flex h-full flex-col'>
 				<Provider>
+					<Header />
 					<main className='grow'>{children}</main>
 				</Provider>
 			</body>
